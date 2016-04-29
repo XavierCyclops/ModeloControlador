012f4352584333a1e8a3b223baa3fd0e67d9e65e
Xavier <Xavier@192.168.23.55>
2016-04-29 13:35:16 -0500

    Conecta rutas y usuarios

---
+++ src/java/DBConex/RutaFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBConex;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Xavier
+ */
+@Stateless
+public class RutaFacade extends AbstractFacade<Ruta> {
+
+    @PersistenceContext(unitName = "TerminusCienciasPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public RutaFacade() {
+        super(Ruta.class);
+    }
+    
+}
